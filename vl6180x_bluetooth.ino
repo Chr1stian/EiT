f27c6775992634395c8dffa902e23885270601e8
Christian Nyvoll <>
2019-03-20 13:53:56 +0100

    Upload of correct Arduino file

---
+++ vl6180x_bluetooth.ino
@@ -0,0 +1,37 @@
+#include "SoftwareSerial.h"
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
+
+Adafruit_VL6180X vl = Adafruit_VL6180X();
+
+SoftwareSerial serial_connection(10, 11);//Create a serial connection with TX and RX on these pins
+#define BUFFER_SIZE 64//This will prevent buffer overruns.
+char inData[BUFFER_SIZE];//This is a character buffer where the data sent by the python script will go.
+char inChar=-1;//Initialie the first character as nothing
+int count=0;//This is the number of lines sent in from the python script
+int i=0;//Arduinos are not the most capable chips in the world so I just create the looping variable once
+void setup()
+{
+   // wait for serial port to open on native usb devices
+  while (!Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL6180x test!");
+  if (! vl.begin()) {
+    Serial.println("Failed to find sensor");
+    while (1);
+  }
+  Serial.println("Sensor found!");
+  
+  Serial.begin(9600);//Initialize communications to the serial monitor in the Arduino IDE
+  serial_connection.begin(9600);//Initialize communications with the bluetooth module
+  serial_connection.println("Ready!!!");//Send something to just start comms. This will never be seen.
+  Serial.println("Started");//Tell the serial monitor that the sketch has started.
+}
+void loop()
+{
+  uint8_t range = vl.readRange();
+  serial_connection.println(range);
+  delay(100);//Pause for a moment 
+}
